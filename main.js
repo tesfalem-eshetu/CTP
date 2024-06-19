de710305e0e78fc223365b379f93a2368a402df3
tesfalem.eshetu.brighter.future <tesfalem.eshetu.brighter.future@gmail.com>
2024-06-19 13:29:03 +0000

    home page prototype completed

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Add JS here
+
+const hamBtn = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamBtn.innerHTML = navLinks.classList.contains('active') ? '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
+    navLinks.classList.contains('active') ? navLinks.style.display = 'block' : navLinks.style.display = 'none';
+});
