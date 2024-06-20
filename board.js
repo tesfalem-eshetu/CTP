8f44856a598a954d90958e4909aca51953b8de2c
tesfalem.eshetu.brighter.future <tesfalem.eshetu.brighter.future@gmail.com>
2024-06-20 15:20:36 +0000

    header  main and footer basic functionlity working for the home page

---
+++ board.js
@@ -0,0 +1,54 @@
+export const board = [
+    {   image: "tesfa.jpg",
+        name_: "Tesfalem <br>Hussien",
+        universty: "Columbia Universty",
+        position: "Co-President"
+    },
+    {
+        image: "bezawit.jpg",
+        name_: "Bezawit <br> Biru",
+        universty: "Columbia Universty",
+        position: "Co-President"
+    },
+    {   image: "abenezer.jpg",
+        name_: "Abenezer<br> Amanuel",
+        universty: "Columbia Universty",
+        position: "Technolgy Team"
+    },
+    {   image: "tsigemariam.jpg",
+        name_: "Tsigemariam <br> Assegid",
+        universty: "Columbia Universty",
+        position: "Technology Team"
+    },
+    {   image: "yeabsira.jpg",
+        name_: "Yeabsira <br>Mohammed",
+        universty: "Harvard Universty",
+        position: "Communications  Team"
+    },
+    {   image: "aida.jpg",
+        name_: "Aida <br> Jemberu",
+        universty: "Columbia Universty",
+        position: "Communications Team"
+    },
+    {   image: "yafet.jpg",
+        name_: "Yafet <br> Hailu ",
+        universty: "Northwestern Universty",
+        position: "Careers Team"
+    },
+    {   image: "ruth.jpg",
+        name_: "Ruth <br> Ukubay ",
+        universty: "Brown <br> Universty",
+        position: "Careers Team"
+    },
+    {   image: "wakuma.jpg",
+        name_: "Wakuma <br> Yebasse",
+        universty: "Columbia Universty",
+        position: "Finance Team"
+    },
+    {   image: "israel.jpg",
+        name_: "Israel <br> Ayele ",
+        universty: "Columbia Universty",
+        position: "Finance Team"
+    },
+
+]
--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-
+import { board } from './board.js';
 const hamBtn = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const theboard = document.querySelector('.theboard');
+const copyright = document.querySelector('.copyright');
+const body = document.querySelector('body');
 
 hamBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     hamBtn.innerHTML = navLinks.classList.contains('active') ? '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
-    navLinks.classList.contains('active') ? navLinks.style.display = 'block' : navLinks.style.display = 'none';
+    navLinks.classList.contains('active') ? navLinks.style.display = 'flex' : navLinks.style.display = 'none';
+    body.classList.toggle('disaable-scroll');
 });
+
+let boardHtml = ""
+board.forEach(function(boardMember) {
+    let {image,name_,universty,position} = boardMember
+    boardHtml += `
+                <div class="individual">
+                <img class="avatar"  src="images/board/${image}" alt="individual avatar">
+                <p class="board-name">${name_}</p>
+                <p class="universty-name">${universty}</p>
+                <p class="position">${position}</p>
+                </div>`
+  });
+theboard.innerHTML = boardHtml;
+copyright.innerHTML = `CTP ETHIOPIA © ${new Date().getFullYear()}`;
